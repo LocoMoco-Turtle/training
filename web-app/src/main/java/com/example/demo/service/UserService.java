bd6281c3e48cf46ddcaf953e19e5cd6e021b97be
LocoMoco-Turtle <t-ishiguro@sang.co.jp>
2024-06-22 14:16:05 +0900

    排他制御

--- web-app/src/main/java/com/example/demo/service/UserService.java
+++ web-app/src/main/java/com/example/demo/service/UserService.java
@@ -1,12 +1,17 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+
+import jakarta.persistence.OptimisticLockException;
+
 
 @Service
 public class UserService {
@@ -20,6 +25,32 @@ public class UserService {
 
 	public User search(Long id) {
 		return userRepository.findById(id).get();
+	}
+
+	public User createUser(User user) {
+		LocalDateTime now = LocalDateTime.now();
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+		return userRepository.save(user);
+	}
+
+	public void deleteUser(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public User updateUser(User user) {
+		User currentUser = userRepository.findOneForUpdate(user.getId());
+
+		if (currentUser.getUpdateDate().equals(user.getUpdateDate())) {
+			LocalDateTime now = LocalDateTime.now();
+			user.setUpdateDate(now);
+			return userRepository.save(user);
+
+		} else {
+			String message = "データが他の方によって更新されたようです。一覧画面に戻ってから再実施してください。";
+			throw new OptimisticLockException(message);
+		}
 	}
 
 }
